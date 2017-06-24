5598092cf7cd93ae9494b6c9459efd40aa320efe
Carlos De la Mora <carlosdelamorac@gmail.com>
2017-06-24 09:02:59 -0600

    Firebase functions

---
+++ functions/index.js
@@ -0,0 +1,48 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+exports.updateStatus = functions.database.ref('/bidIdStatus/{bidIdStatus}').onWrite( event => {
+
+     const snapshot = event.data.val();
+     const authorOfTheBidFirebaseId = snapshot.authorOfTheBid;
+     const theOtherUserFirebaseId = snapshot.otherUser;
+     const status = snapshot.status;
+     const bidId = snapshot.bidId;
+     const lastOneToWrite = snapshot.lastOneToWrite;
+     console.log('status is', status );
+     const pathForAuthor = '/Users/' + authorOfTheBidFirebaseId + '/Bid/' + bidId;
+     const pathForTheOther = '/Users/' + theOtherUserFirebaseId + '/Bid/' + bidId;
+
+     if (status !== 'active' && status !== 'halfComplete'){
+         updateStatus(pathForAuthor,status);
+         updateStatus(pathForTheOther, status);
+     };
+
+     if( status === 'halfComplete'){
+         const updateToComplete = (lastOneToWrite === authorOfTheBidFirebaseId) ? pathForAuthor: pathForTheOther;
+         const updateToHalfComplete = (lastOneToWrite !== authorOfTheBidFirebaseId) ? pathForAuthor: pathForTheOther;
+         updateStatus(updateToComplete, "complete");
+         updateStatus(updateToHalfComplete, "halfComple");
+     };
+
+     return ;
+});
+
+function updateStatus(path, estatus){
+  console.log('path',path, ',estatus', estatus);
+   return admin.database().ref(path).update({'/offer/offerStatus':estatus}).then(function(response){
+      console.log(response, Object.prototype.toString.call(response));
+
+   }).catch(function(error){
+     console.log(error, Object.prototype.toString.call(error));
+   });
+}
