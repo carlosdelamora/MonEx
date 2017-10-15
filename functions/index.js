7e430f7fa580327f231615f6b5a62c60c1807a09
Carlos De la Mora <carlosdelamorac@gmail.com>
2017-10-14 19:29:37 -0600

    bug: Counteroffer only whrites with messages received

--- functions/index.js
+++ functions/index.js
@@ -22,7 +22,9 @@ exports.updateStatus = functions.database.ref('/bidIdStatus/{bidIdStatus}').onWr
      const pathForAuthor = '/Users/' + authorOfTheBidFirebaseId + '/Bid/' + bidId;
      const pathForTheOther = '/Users/' + theOtherUserFirebaseId + '/Bid/' + bidId;
 
-     if (status !== 'active' && status !== 'halfComplete'){
+     //if the status is different from active halfCpmplete or counteroffer,
+     //we do not update the status, we need to wait for the update by messages
+     if (status !== 'active' && status !== 'halfComplete' && 'counterOffer'){
          updateStatus(pathForAuthor,status);
          updateStatus(pathForTheOther, status);
      };
